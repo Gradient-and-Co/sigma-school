3fe81949d60db50e33b4aa0e5109c5a77ef7e962
Pavel Shpakovskiy <shpakovskiipa@gmail.com>
2025-04-13 21:27:41 +0300

    backend #8: add repository unit test with sqlmock package

---
+++ internal/adapter/repository/postgres/test/course.go
@@ -0,0 +1,62 @@
+package test
+
+import (
+	"github.com/Gradient-and-Co/sigma-school/internal/core/domain"
+)
+
+type CourseBuilder struct {
+	course domain.Course
+}
+
+func NewCourseBuilder() *CourseBuilder {
+	return &CourseBuilder{
+		course: domain.Course{
+			ID:       domain.NewID(),
+			SchoolID: domain.NewID(),
+			Name:     "course name",
+			Level:    3,
+			Price:    1000,
+			Language: "english",
+			Status:   domain.CourseDraft,
+		},
+	}
+}
+
+func (b *CourseBuilder) WithID(id domain.ID) *CourseBuilder {
+	b.course.ID = id
+	return b
+}
+
+func (b *CourseBuilder) WithSchoolID(schoolID domain.ID) *CourseBuilder {
+	b.course.SchoolID = schoolID
+	return b
+}
+
+func (b *CourseBuilder) WithName(name string) *CourseBuilder {
+	b.course.Name = name
+	return b
+}
+
+func (b *CourseBuilder) WithLevel(level int) *CourseBuilder {
+	b.course.Level = level
+	return b
+}
+
+func (b *CourseBuilder) WithPrice(price int64) *CourseBuilder {
+	b.course.Price = price
+	return b
+}
+
+func (b *CourseBuilder) WithLanguage(language string) *CourseBuilder {
+	b.course.Language = language
+	return b
+}
+
+func (b *CourseBuilder) WithStatus(status domain.CourseStatus) *CourseBuilder {
+	b.course.Status = status
+	return b
+}
+
+func (b *CourseBuilder) Build() domain.Course {
+	return b.course
+}
