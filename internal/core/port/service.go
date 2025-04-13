c0bb9bbd5cbd778d73214186ec0fa5b7b72e980c
Pavel Shpakovskiy <shpakovskiipa@gmail.com>
2025-04-13 20:02:54 +0300

    backend #6: add course service and school, lesson repository interfaces

--- internal/core/port/service.go
+++ internal/core/port/service.go
@@ -14,3 +14,22 @@ type IUserService interface {
 	Update(ctx context.Context, userID domain.ID, param UpdateUserParam) (domain.User, error)
 	Delete(ctx context.Context, userID domain.ID) error
 }
+
+type ICourseService interface {
+	FindAll(ctx context.Context) ([]domain.Course, error)
+	FindByID(ctx context.Context, courseID domain.ID) (domain.Course, error)
+	FindStudentCourses(ctx context.Context, studentID domain.ID) ([]domain.Course, error)
+	FindTeacherCourses(ctx context.Context, teacherID domain.ID) ([]domain.Course, error)
+	FindCourseTeachers(ctx context.Context, courseID domain.ID) ([]domain.User, error)
+	IsCourseStudent(ctx context.Context, studentID, courseID domain.ID) (bool, error)
+	IsCourseTeacher(ctx context.Context, teacherID, courseID domain.ID) (bool, error)
+	AddCourseStudent(ctx context.Context, studentID, courseID domain.ID) error
+	AddCourseTeacher(ctx context.Context, teacherID, courseID domain.ID) error
+	ConfirmDraftCourse(ctx context.Context, courseID domain.ID) []error
+	PublishReadyCourse(ctx context.Context, courseID domain.ID) error
+	CreateSchoolCourse(ctx context.Context, schoolID domain.ID,
+		param CreateCourseParam) (domain.Course, error)
+	Update(ctx context.Context, courseID domain.ID,
+		param UpdateCourseParam) (domain.Course, error)
+	Delete(ctx context.Context, courseID domain.ID) error
+}
