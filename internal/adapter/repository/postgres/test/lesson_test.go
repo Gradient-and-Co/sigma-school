9b6ea438551481ef10c8adfe6b9008adb56cdd11
Pavel Shpakovskiy <shpakovskiipa@gmail.com>
2025-04-13 21:27:41 +0300

    backend #8: add repository unit test with sqlmock package

---
+++ internal/adapter/repository/postgres/test/lesson_test.go
@@ -0,0 +1,326 @@
+package test
+
+import (
+	"context"
+	"database/sql"
+	"github.com/DATA-DOG/go-sqlmock"
+	repository "github.com/Gradient-and-Co/sigma-school/internal/adapter/repository/postgres"
+	"github.com/Gradient-and-Co/sigma-school/internal/adapter/repository/postgres/entity"
+	"github.com/Gradient-and-Co/sigma-school/internal/core/domain"
+	"github.com/Gradient-and-Co/sigma-school/internal/core/errs"
+	"github.com/jmoiron/sqlx"
+	"github.com/ozontech/allure-go/pkg/framework/provider"
+	"github.com/ozontech/allure-go/pkg/framework/suite"
+	"testing"
+)
+
+type LessonSuite struct {
+	suite.Suite
+}
+
+func NewLessonRepository() (port.ILessonRepository, sqlmock.Sqlmock) {
+	db, mock, _ := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	conn := sqlx.NewDb(db, "pgx")
+	repo := repository.NewLessonRepo(conn)
+	return repo, mock
+}
+
+// FindAll Suite
+type LessonFindAllSuite struct {
+	LessonSuite
+}
+
+func (s *LessonFindAllSuite) LessonFindAllSuccessRepositoryMock(mock sqlmock.Sqlmock, lesson domain.Lesson) {
+	pgLesson := entity.NewPgLesson(lesson)
+	expectedRows := sqlmock.NewRows(EntityColumns(pgLesson))
+	expectedRows.AddRow(EntityValues(pgLesson)...)
+	mock.ExpectQuery(repository.LessonFindAllQuery).WillReturnRows(expectedRows)
+}
+
+func (s *LessonFindAllSuite) TestFindAll_Success(t provider.T) {
+	t.Parallel()
+	t.Title("Lesson repository find all success")
+	repo, mock := NewLessonRepository()
+	lesson := NewLessonBuilder().Build()
+	s.LessonFindAllSuccessRepositoryMock(mock, lesson)
+	lessons, err := repo.FindAll(context.Background())
+	t.Assert().Nil(err)
+	t.Assert().Equal(lessons[0].Title, lesson.Title)
+}
+
+func (s *LessonFindAllSuite) LessonFindAllFailureRepositoryMock(mock sqlmock.Sqlmock) {
+	mock.ExpectQuery(repository.LessonFindAllQuery).WillReturnError(sql.ErrNoRows)
+}
+
+func (s *LessonFindAllSuite) TestFindAll_Failure(t provider.T) {
+	t.Parallel()
+	t.Title("Lesson repository find all failure")
+	repo, mock := NewLessonRepository()
+	s.LessonFindAllFailureRepositoryMock(mock)
+	_, err := repo.FindAll(context.Background())
+	t.Assert().ErrorIs(err, errs.ErrNotExist)
+}
+
+func TestLessonFindAllSuite(t *testing.T) {
+	suite.RunNamedSuite(t, "Lesson repository find all", new(LessonFindAllSuite))
+}
+
+type LessonFindByIDSuite struct {
+	LessonSuite
+}
+
+func (s *LessonFindByIDSuite) LessonFindByIDSuccessRepositoryMock(mock sqlmock.Sqlmock, lesson domain.Lesson) {
+	pgLesson := entity.NewPgLesson(lesson)
+	expectedRows := sqlmock.NewRows(EntityColumns(pgLesson)).
+		AddRow(EntityValues(pgLesson)...)
+	mock.ExpectQuery(repository.LessonFindByIDQuery).WithArgs(pgLesson.ID).WillReturnRows(expectedRows)
+}
+
+func (s *LessonFindByIDSuite) TestFindByID_Success(t provider.T) {
+	t.Parallel()
+	t.Title("Lesson repository find by id success")
+	repo, mock := NewLessonRepository()
+	lesson := NewLessonBuilder().Build()
+	s.LessonFindByIDSuccessRepositoryMock(mock, lesson)
+	foundLesson, err := repo.FindByID(context.Background(), lesson.ID)
+	t.Assert().Nil(err)
+	t.Assert().Equal(foundLesson.ID, lesson.ID)
+}
+
+func (s *LessonFindByIDSuite) LessonFindByIDFailureRepositoryMock(mock sqlmock.Sqlmock) {
+	mock.ExpectQuery(repository.LessonFindByIDQuery).WillReturnError(sql.ErrNoRows)
+}
+
+func (s *LessonFindByIDSuite) TestFindByID_Failure(t provider.T) {
+	t.Parallel()
+	t.Title("Lesson repository find by id failure")
+	repo, mock := NewLessonRepository()
+	s.LessonFindByIDFailureRepositoryMock(mock)
+	_, err := repo.FindByID(context.Background(), domain.NewID())
+	t.Assert().ErrorIs(err, errs.ErrNotExist)
+}
+
+func TestLessonFindByIDSuite(t *testing.T) {
+	suite.RunNamedSuite(t, "Lesson repository find by id", new(LessonFindByIDSuite))
+}
+
+type LessonFindLessonTestsSuite struct {
+	LessonSuite
+}
+
+func (s *LessonFindLessonTestsSuite) LessonFindLessonTestsSuccessRepositoryMock(mock sqlmock.Sqlmock,
+	tests []domain.Test, lessonID domain.ID) {
+	pgTest1 := entity.NewPgTest(tests[0])
+	pgTest2 := entity.NewPgTest(tests[1])
+	expectedRows := sqlmock.NewRows(EntityColumns(pgTest1))
+	expectedRows.AddRow(EntityValues(pgTest1)...)
+	expectedRows.AddRow(EntityValues(pgTest2)...)
+	mock.ExpectQuery(repository.LessonFindLessonTestsQuery).WithArgs(lessonID).WillReturnRows(expectedRows)
+}
+
+func (s *LessonFindLessonTestsSuite) TestFindLessonTests_Success(t provider.T) {
+	t.Parallel()
+	t.Title("Lesson repository find lesson tests success")
+	repo, mock := NewLessonRepository()
+	lessonID := domain.NewID()
+	tests := []domain.Test{NewTestBuilder().Build(), NewTestBuilder().Build()}
+	s.LessonFindLessonTestsSuccessRepositoryMock(mock, tests, lessonID)
+	actual, err := repo.FindLessonTests(context.Background(), lessonID)
+	t.Assert().Nil(err)
+	t.Assert().Equal(tests[0], actual[0])
+	t.Assert().Equal(tests[1], actual[1])
+}
+
+func (s *LessonFindLessonTestsSuite) LessonFindLessonTestsFailureRepositoryMock(mock sqlmock.Sqlmock) {
+	mock.ExpectQuery(repository.LessonFindLessonTestsQuery).WillReturnError(sql.ErrNoRows)
+}
+
+func (s *LessonFindLessonTestsSuite) TestFindLessonTests_Failure(t provider.T) {
+	t.Parallel()
+	t.Title("Lesson repository find lesson tests failure")
+	repo, mock := NewLessonRepository()
+	userID := domain.NewID()
+	s.LessonFindLessonTestsFailureRepositoryMock(mock)
+	_, err := repo.FindLessonTests(context.Background(), userID)
+	t.Assert().ErrorIs(err, errs.ErrNotExist)
+}
+
+func TestLessonFindLessonTestsSuite(t *testing.T) {
+	suite.RunNamedSuite(t, "Lesson repository find lesson tests", new(LessonFindLessonTestsSuite))
+}
+
+type LessonFindCourseLessonsSuite struct {
+	LessonSuite
+}
+
+func (s *LessonFindCourseLessonsSuite) LessonFindCourseLessonsSuccessRepositoryMock(mock sqlmock.Sqlmock,
+	lesson domain.Lesson, courseID domain.ID) {
+	pgLesson := entity.NewPgLesson(lesson)
+	expectedRows := sqlmock.NewRows(EntityColumns(pgLesson))
+	expectedRows.AddRow(EntityValues(pgLesson)...)
+	mock.ExpectQuery(repository.LessonFindCourseLessonsQuery).WithArgs(courseID).WillReturnRows(expectedRows)
+}
+
+func (s *LessonFindCourseLessonsSuite) TestFindCourseLessons_Success(t provider.T) {
+	t.Parallel()
+	t.Title("Lesson repository find course lessons success")
+	repo, mock := NewLessonRepository()
+	lesson := NewLessonBuilder().Build()
+	courseID := domain.NewID()
+	s.LessonFindCourseLessonsSuccessRepositoryMock(mock, lesson, courseID)
+	lessons, err := repo.FindCourseLessons(context.Background(), courseID)
+	t.Assert().Nil(err)
+	t.Assert().Equal(lessons[0].Title, lesson.Title)
+}
+
+func (s *LessonFindCourseLessonsSuite) LessonFindCourseLessonsFailureRepositoryMock(mock sqlmock.Sqlmock) {
+	mock.ExpectQuery(repository.LessonFindCourseLessonsQuery).WillReturnError(sql.ErrNoRows)
+}
+
+func (s *LessonFindCourseLessonsSuite) TestFindCourseLessons_Failure(t provider.T) {
+	t.Parallel()
+	t.Title("Lesson repository find course lessons failure")
+	repo, mock := NewLessonRepository()
+	courseID := domain.NewID()
+	s.LessonFindCourseLessonsFailureRepositoryMock(mock)
+	_, err := repo.FindCourseLessons(context.Background(), courseID)
+	t.Assert().ErrorIs(err, errs.ErrNotExist)
+}
+
+func TestLessonFindCourseLessonsSuite(t *testing.T) {
+	suite.RunNamedSuite(t, "Lesson repository find course lessons", new(LessonFindCourseLessonsSuite))
+}
+
+type LessonCreateSuite struct {
+	LessonSuite
+}
+
+func (s *LessonCreateSuite) LessonCreateSuccessRepositoryMock(mock sqlmock.Sqlmock, lesson domain.Lesson) {
+	pgLesson := entity.NewPgLesson(lesson)
+	mock.ExpectBegin()
+	queryString := InsertQueryString(pgLesson, "lesson")
+	mock.ExpectExec(queryString).
+		WithArgs(EntityValues(pgLesson)...).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectCommit()
+	expectedRows := sqlmock.NewRows(EntityColumns(pgLesson)).
+		AddRow(EntityValues(pgLesson)...)
+	mock.ExpectQuery(repository.LessonFindByIDQuery).WithArgs(pgLesson.ID).WillReturnRows(expectedRows)
+}
+
+func (s *LessonCreateSuite) TestCreate_Success(t provider.T) {
+	t.Parallel()
+	t.Title("Lesson repository create lesson success")
+	repo, mock := NewLessonRepository()
+	lesson := NewLessonBuilder().Build()
+	s.LessonCreateSuccessRepositoryMock(mock, lesson)
+	createdLesson, err := repo.Create(context.Background(), lesson)
+	t.Assert().Nil(err)
+	t.Assert().Equal(createdLesson.Title, lesson.Title)
+}
+
+func (s *LessonCreateSuite) LessonCreateFailureRepositoryMock(mock sqlmock.Sqlmock) {
+	mock.ExpectBegin()
+	queryString := InsertQueryString(entity.PgLesson{}, "lesson")
+	mock.ExpectExec(queryString).WillReturnError(sql.ErrConnDone)
+}
+
+func (s *LessonCreateSuite) TestCreate_Failure(t provider.T) {
+	t.Parallel()
+	t.Title("Lesson repository create lesson failure")
+	repo, mock := NewLessonRepository()
+	lesson := NewLessonBuilder().Build()
+	s.LessonCreateFailureRepositoryMock(mock)
+	_, err := repo.Create(context.Background(), lesson)
+	t.Assert().ErrorIs(err, errs.ErrPersistenceFailed)
+}
+
+func TestLessonCreateSuite(t *testing.T) {
+	suite.RunNamedSuite(t, "Lesson repository create lesson", new(LessonCreateSuite))
+}
+
+type LessonUpdateSuite struct {
+	LessonSuite
+}
+
+func (s *LessonUpdateSuite) LessonUpdateSuccessRepositoryMock(mock sqlmock.Sqlmock, lesson domain.Lesson) {
+	pgLesson := entity.NewPgLesson(lesson)
+	mock.ExpectBegin()
+	queryString := UpdateQueryString(pgLesson, "lesson")
+	values := append(EntityValues(pgLesson), pgLesson.ID)
+	mock.ExpectExec(queryString).
+		WithArgs(values...).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectCommit()
+
+	expectedRows := sqlmock.NewRows(EntityColumns(pgLesson)).
+		AddRow(EntityValues(pgLesson)...)
+	mock.ExpectQuery(repository.LessonFindByIDQuery).WithArgs(lesson.ID).WillReturnRows(expectedRows)
+}
+
+func (s *LessonUpdateSuite) TestUpdate_Success(t provider.T) {
+	t.Parallel()
+	t.Title("Lesson repository update lesson success")
+	repo, mock := NewLessonRepository()
+	lesson := NewLessonBuilder().Build()
+	s.LessonUpdateSuccessRepositoryMock(mock, lesson)
+	updatedLesson, err := repo.Update(context.Background(), lesson)
+	t.Assert().Nil(err)
+	t.Assert().Equal(updatedLesson.Title, lesson.Title)
+}
+
+func (s *LessonUpdateSuite) LessonUpdateFailureRepositoryMock(mock sqlmock.Sqlmock) {
+	mock.ExpectBegin()
+	queryString := UpdateQueryString(entity.PgLesson{}, "lesson")
+	mock.ExpectExec(queryString).WillReturnError(sql.ErrConnDone)
+}
+
+func (s *LessonUpdateSuite) TestUpdate_Failure(t provider.T) {
+	t.Parallel()
+	t.Title("Lesson repository update lesson failure")
+	repo, mock := NewLessonRepository()
+	lesson := NewLessonBuilder().Build()
+	s.LessonUpdateFailureRepositoryMock(mock)
+	_, err := repo.Update(context.Background(), lesson)
+	t.Assert().ErrorIs(err, errs.ErrUpdateFailed)
+}
+
+func TestLessonUpdateSuite(t *testing.T) {
+	suite.RunNamedSuite(t, "Lesson repository update lesson", new(LessonUpdateSuite))
+}
+
+type LessonDeleteSuite struct {
+	LessonSuite
+}
+
+func (s *LessonDeleteSuite) LessonDeleteSuccessRepositoryMock(mock sqlmock.Sqlmock, lessonID domain.ID) {
+	mock.ExpectExec(repository.LessonDeleteQuery).WithArgs(lessonID).WillReturnResult(sqlmock.NewResult(1, 1))
+}
+
+func (s *LessonDeleteSuite) TestDelete_Success(t provider.T) {
+	t.Parallel()
+	t.Title("Lesson repository delete lesson success")
+	repo, mock := NewLessonRepository()
+	lesson := NewLessonBuilder().Build()
+	s.LessonDeleteSuccessRepositoryMock(mock, lesson.ID)
+	err := repo.Delete(context.Background(), lesson.ID)
+	t.Assert().Nil(err)
+}
+
+func (s *LessonDeleteSuite) LessonDeleteFailureRepositoryMock(mock sqlmock.Sqlmock) {
+	mock.ExpectExec(repository.LessonDeleteQuery).WillReturnError(sql.ErrConnDone)
+}
+
+func (s *LessonDeleteSuite) TestDelete_Failure(t provider.T) {
+	t.Parallel()
+	t.Title("Lesson repository delete lesson failure")
+	repo, mock := NewLessonRepository()
+	lesson := NewLessonBuilder().Build()
+	s.LessonDeleteFailureRepositoryMock(mock)
+	err := repo.Delete(context.Background(), lesson.ID)
+	t.Assert().ErrorIs(err, errs.ErrDeleteFailed)
+}
+
+func TestLessonDeleteSuite(t *testing.T) {
+	suite.RunNamedSuite(t, "Lesson repository delete lesson", new(LessonDeleteSuite))
+}
---
+++ internal/adapter/repository/postgres/test/stat_test.go
@@ -0,0 +1,160 @@
+package test
+
+import (
+	"context"
+	"database/sql"
+	"github.com/DATA-DOG/go-sqlmock"
+	repository "github.com/Gradient-and-Co/sigma-school/internal/adapter/repository/postgres"
+	"github.com/Gradient-and-Co/sigma-school/internal/adapter/repository/postgres/entity"
+	"github.com/Gradient-and-Co/sigma-school/internal/core/domain"
+	"github.com/Gradient-and-Co/sigma-school/internal/core/errs"
+	"github.com/jmoiron/sqlx"
+	"github.com/ozontech/allure-go/pkg/framework/provider"
+	"github.com/ozontech/allure-go/pkg/framework/suite"
+	"testing"
+)
+
+type StatSuite struct {
+	suite.Suite
+}
+
+func NewStatRepository() (port.IStatRepository, sqlmock.Sqlmock) {
+	db, mock, _ := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	conn := sqlx.NewDb(db, "pgx")
+	repo := repository.NewStatRepo(conn)
+	return repo, mock
+}
+
+type StatFindLessonStatSuite struct {
+	StatSuite
+}
+
+func (s *StatFindLessonStatSuite) StatFindLessonStatSuccessRepositoryMock(mock sqlmock.Sqlmock,
+	stat domain.LessonStat, userID, lessonID domain.ID) {
+	pgStat := entity.NewPgLessonStat(stat)
+	expectedRows := sqlmock.NewRows(EntityColumns(pgStat))
+	expectedRows.AddRow(EntityValues(pgStat)...)
+	mock.ExpectQuery(repository.StatFindByUserLessonQuery).WithArgs(userID, lessonID).WillReturnRows(expectedRows)
+	mock.ExpectQuery(repository.StatFindLessonTestsQuery).WithArgs(lessonID).WillReturnError(sql.ErrNoRows)
+}
+
+func (s *StatFindLessonStatSuite) TestFindLessonStat_Success(t provider.T) {
+	t.Parallel()
+	t.Title("Stat repository find lesson stat success")
+	repo, mock := NewStatRepository()
+	stat := NewLessonStatBuilder().Build()
+	userID := domain.NewID()
+	lessonID := domain.NewID()
+	s.StatFindLessonStatSuccessRepositoryMock(mock, stat, userID, lessonID)
+	actual, err := repo.FindLessonStat(context.Background(), userID, lessonID)
+	t.Assert().Nil(err)
+	t.Assert().Equal(stat.Score, actual.Score)
+}
+
+func (s *StatFindLessonStatSuite) StatFindLessonStatFailureRepositoryMock(mock sqlmock.Sqlmock) {
+	mock.ExpectQuery(repository.StatFindByUserLessonQuery).WillReturnError(sql.ErrNoRows)
+}
+
+func (s *StatFindLessonStatSuite) TestFindLessonStat_Failure(t provider.T) {
+	t.Parallel()
+	t.Title("Stat repository find lesson stat failure")
+	repo, mock := NewStatRepository()
+	userID := domain.NewID()
+	lessonID := domain.NewID()
+	s.StatFindLessonStatFailureRepositoryMock(mock)
+	_, err := repo.FindLessonStat(context.Background(), userID, lessonID)
+	t.Assert().ErrorIs(err, errs.ErrNotExist)
+}
+
+func TestStatFindLessonStatSuite(t *testing.T) {
+	suite.RunNamedSuite(t, "Stat repository find lesson stat", new(StatFindLessonStatSuite))
+}
+
+type StatCreateSuite struct {
+	StatSuite
+}
+
+func (s *StatCreateSuite) StatCreateSuccessRepositoryMock(mock sqlmock.Sqlmock, stat domain.LessonStat) {
+	pgStat := entity.NewPgLessonStat(stat)
+	mock.ExpectBegin()
+	queryString := InsertQueryString(pgStat, "lesson_stat")
+	mock.ExpectExec(queryString).
+		WithArgs(EntityValues(pgStat)...).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectCommit()
+}
+
+func (s *StatCreateSuite) TestCreate_Success(t provider.T) {
+	t.Parallel()
+	t.Title("Stat repository create lesson stat success")
+	repo, mock := NewStatRepository()
+	stat := NewLessonStatBuilder().Build()
+	s.StatCreateSuccessRepositoryMock(mock, stat)
+	err := repo.CreateLessonStat(context.Background(), stat)
+	t.Assert().Nil(err)
+}
+
+func (s *StatCreateSuite) StatCreateFailureRepositoryMock(mock sqlmock.Sqlmock) {
+	mock.ExpectBegin()
+	queryString := InsertQueryString(entity.PgLessonStat{}, "lesson_stat")
+	mock.ExpectExec(queryString).WillReturnError(sql.ErrConnDone)
+}
+
+func (s *StatCreateSuite) TestCreate_Failure(t provider.T) {
+	t.Parallel()
+	t.Title("Stat repository create lesson stat failure")
+	repo, mock := NewStatRepository()
+	stat := NewLessonStatBuilder().Build()
+	s.StatCreateFailureRepositoryMock(mock)
+	err := repo.CreateLessonStat(context.Background(), stat)
+	t.Assert().ErrorIs(err, errs.ErrPersistenceFailed)
+}
+
+func TestStatCreateSuite(t *testing.T) {
+	suite.RunNamedSuite(t, "Stat repository create lesson stat", new(StatCreateSuite))
+}
+
+type StatUpdateSuite struct {
+	StatSuite
+}
+
+func (s *StatUpdateSuite) StatUpdateSuccessRepositoryMock(mock sqlmock.Sqlmock, stat domain.LessonStat) {
+	pgStat := entity.NewPgLessonStat(stat)
+	mock.ExpectBegin()
+	queryString := UpdateQueryString(pgStat, "lesson_stat")
+	values := append(EntityValues(pgStat), pgStat.ID)
+	mock.ExpectExec(queryString).
+		WithArgs(values...).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectCommit()
+}
+
+func (s *StatUpdateSuite) TestUpdate_Success(t provider.T) {
+	t.Parallel()
+	t.Title("Stat repository update lesson stat success")
+	repo, mock := NewStatRepository()
+	stat := NewLessonStatBuilder().Build()
+	s.StatUpdateSuccessRepositoryMock(mock, stat)
+	err := repo.UpdateLessonStat(context.Background(), stat)
+	t.Assert().Nil(err)
+}
+
+func (s *StatUpdateSuite) StatUpdateFailureRepositoryMock(mock sqlmock.Sqlmock) {
+	mock.ExpectBegin()
+	queryString := UpdateQueryString(entity.PgLessonStat{}, "lesson_stat")
+	mock.ExpectExec(queryString).WillReturnError(sql.ErrConnDone)
+}
+
+func (s *StatUpdateSuite) TestUpdate_Failure(t provider.T) {
+	t.Parallel()
+	t.Title("Stat repository update lesson stat failure")
+	repo, mock := NewStatRepository()
+	stat := NewLessonStatBuilder().Build()
+	s.StatUpdateFailureRepositoryMock(mock)
+	err := repo.UpdateLessonStat(context.Background(), stat)
+	t.Assert().ErrorIs(err, errs.ErrUpdateFailed)
+}
+
+func TestStatUpdateSuite(t *testing.T) {
+	suite.RunNamedSuite(t, "Stat repository update lesson stat", new(StatUpdateSuite))
+}
