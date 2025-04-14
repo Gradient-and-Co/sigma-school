56a825c628520a06a7aabd12f15438bd3f3c18f7
Pavel Shpakovskiy <shpakovskiipa@gmail.com>
2025-04-14 10:53:42 +0300

    backend #18: add minio and redis packages

---
+++ pkg/database/postgres/postgres.go
@@ -0,0 +1,51 @@
+package postgres
+
+import (
+	"fmt"
+	_ "github.com/jackc/pgx/v4/stdlib"
+	"github.com/jmoiron/sqlx"
+	"go.uber.org/zap"
+	"time"
+)
+
+type Config struct {
+	Host     string
+	Port     string
+	Database string
+	User     string
+	Password string
+}
+
+const (
+	maxConn         = 100
+	maxConnIdleTime = 1 * time.Minute
+	maxConnLifetime = 3 * time.Minute
+)
+
+func NewPostgresDB(cfg *Config, logger *zap.Logger) (*sqlx.DB, error) {
+	connectionString := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s",
+		cfg.Host,
+		cfg.Port,
+		cfg.User,
+		cfg.Database,
+		cfg.Password,
+	)
+
+	db, err := sqlx.Connect("pgx", connectionString)
+	if err != nil {
+		logger.Fatal("failed to connect postgres db: %s", zap.String("conn string", connectionString))
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(maxConn)
+	db.SetConnMaxLifetime(maxConnLifetime)
+	db.SetConnMaxIdleTime(maxConnIdleTime)
+
+	err = db.Ping()
+	if err != nil {
+		logger.Fatal("failed to ping postgres db: %s", zap.String("conn string", connectionString))
+		return nil, err
+	}
+
+	return db, nil
+}
